9d54be0227e089f667718fce0f87658ea5cc2cca
H-arshit <harshitsinghal33@gmail.com>
2017-10-26 16:09:56 +0530

    Added Username

---
+++ app/src/main/java/com/example/harshit/apiuse/RequestData.java
@@ -0,0 +1,60 @@
+package com.example.harshit.apiuse;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by harshit on 13/10/17.
+ */
+
+public class RequestData {
+
+
+
+    private String email ;
+
+
+    private String name;
+
+    private String password;
+
+
+
+
+    private String phone_no;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone_no() {
+        return phone_no;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhone_no(String phone_no) {
+        this.phone_no = phone_no;
+    }
+
+}
+
